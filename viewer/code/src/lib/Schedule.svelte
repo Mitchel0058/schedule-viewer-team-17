43234b0522edc8b137558a22643d184945f5f025
Jasonjason10 <ofil0001@hz.nl>
2023-07-06 19:49:03 +0200

    you can now see one week at a time instead of all of them

--- viewer/code/src/lib/Schedule.svelte
+++ viewer/code/src/lib/Schedule.svelte
@@ -1,21 +1,50 @@
 <script>
+    import { onMount } from 'svelte';
     import Week from "./components/Week.svelte";
-    const fetchSchedule = (async () => {
-        const response = await fetch("http://localhost:3010/schedule");
-        console.log("wait what?", response);
-        const jsonResponse = await response.json();
-        //console.log(jsonResponse);
-        return jsonResponse;
-    })();
-</script>
 
-{#await fetchSchedule}
-    <p>...waiting</p>
-{:then data}
-    <!-- The week -->
-    {#each data.data as week}
-        <Week week={week} />
-    {/each}
-{:catch error}
-    <p>An error occurred!</p>
-{/await}
+    let data;
+    let currentWeekIndex = 0;
+
+    const fetchSchedule = async () => {
+        const response = await fetch("http://localhost:3010/schedule");
+        const jsonResponse = await response.json();
+        data = jsonResponse.data;
+    };
+
+    onMount(fetchSchedule);
+
+    function nextWeek() {
+        currentWeekIndex = (currentWeekIndex + 1) % data.length;
+    }
+
+    function previousWeek() {
+        currentWeekIndex = (currentWeekIndex - 1 + data.length) % data.length;
+    }
+</script>
+<style>
+    .button-container {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+    }
+
+    .button-container button {
+        border: 2px solid blue;
+        background-color: rgb(12, 186, 245);
+        color: white;
+        padding: 10px 20px;
+        margin: 0 10px;
+        cursor: pointer;
+    }
+</style>
+
+
+<div class="button-container">
+    <button on:click={previousWeek}>Previous Week</button>
+    <button on:click={nextWeek}>Next Week</button>
+</div>
+
+
+{#if data}
+    <Week week={data[currentWeekIndex]} />
+{/if}
