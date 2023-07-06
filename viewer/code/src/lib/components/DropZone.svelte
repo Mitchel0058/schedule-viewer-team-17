fb6f83554a26eda43ba4dd0e909c4854c87243fa
Zapplightforce <pers0010@hz.nl>
2023-07-06 23:29:03 +0200

    working drag and drop

--- viewer/code/src/lib/components/DropZone.svelte
+++ viewer/code/src/lib/components/DropZone.svelte
@@ -11,9 +11,32 @@
         files.accepted = [...files.accepted, ...acceptedFiles];
         files.rejected = [...files.rejected, ...fileRejections];
     }
+
+    async function uploadFiles() {
+        const uploadPromises = files.accepted.map(file => {
+            const formData = new FormData();
+            formData.append('excelFile', file);
+
+            return fetch('http://localhost:3010/excelToJson', {
+                method: 'POST',
+                body: formData,
+            });
+        });
+
+        try {
+            const responses = await Promise.all(uploadPromises);
+            const data = await Promise.all(responses.map(response => response.json()));
+
+            // Once all files have been uploaded successfully, clear the accepted files list
+            files = { ...files, accepted: [] };
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
 </script>
 
-<Dropzone on:drop={handleFilesSelect} />
+<Dropzone accept=".xls,.xlsx" on:drop={handleFilesSelect} />
+<button on:click={uploadFiles}>Upload</button>
 <ol>
     {#each files.accepted as item}
         <li>{item.name}</li>
